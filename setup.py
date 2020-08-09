367115e5048205bdf8fb8f222b62cf2278746b1a
Omri Rozenzaft <omrirz@gmail.com>
2020-08-09 23:38:21 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="masav",
-    version="0.0.1",
+    version="0.0.2",
     author="Omri Rozenzaft",
     author_email="omrirz@gmail.com",
     description="Python API for Israeli Masav payments",
